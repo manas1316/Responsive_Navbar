024011e5fa1edd71cca404d6cd7a209091a6c1f5
manas1316 <valorant1316mk@gmail.com>
2021-02-09 14:13:01 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav_links");
+    const navLinks = document.querySelectorAll('.nav_links li');
+
+
+    burger.addEventListener('click', () => {
+        // Toggle Nav
+        nav.classList.toggle('nav-active')
+
+        // Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.5}s`;
+            }
+        });
+        // burger animation
+        burger.classList.toggle(`toggle`);
+    });
+}
+
+navSlide();
